bac06b23a4c31cf57ff7dff15112e572df21e333
Ubaid7867 <78679270+Ubaid7867@users.noreply.github.com>
2021-02-09 13:17:18 +0530

    Assignment of Day3 submited

---
+++ Assignment 1_day3.py
@@ -0,0 +1,17 @@
+x = input("Enter the altitude of the plane:")
+a = int(x)
+print(a)
+
+if a < 1000:
+    print("You Should land now safely")
+
+if a > 5000:
+   print('go around and try later ')
+
+elif a > 1000 and a < 5000:
+     print("Come Down to 1000ft")
+
+
+
+
+
